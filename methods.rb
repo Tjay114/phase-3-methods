484208122518cc2820374c137a296f464f6ccc62
Terormumuli <terahmumuli@gmail.com>
2022-08-14 04:18:00 +0300

    ruby methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+    return "Hello,programmer!"
+end
+
+def greet (name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default ()
+    puts "Hello, programmer!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (numb1, numb2)
+    return numb1 + numb2
+end
+
+def halve (number)
+   number / 2
+end
+    if number != "number"
+        return nil
+    end
+end
